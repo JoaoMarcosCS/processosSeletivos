ae48e1920e701ec697777f17eb4201bf64d7b3e5
João Marcos <jmcsjoaomarcos@gmail.com>
2024-03-19 21:06:09 +0000

    feat: Corrigindo o layout

--- app/footer/footer.tsx
+++ app/footer/footer.tsx
@@ -7,17 +7,21 @@ import {
     MenubarShortcut,
     MenubarTrigger,
 } from "@/components/ui/menubar"
-import {Plus} from 'lucide-react'
+import {Separator} from "@/components/ui/separator"
+import {Plus, Eye} from 'lucide-react'
 
 
 
 export const Footer = () => {
     return (
+      <div className="flex justify-center items-center w-full fixed bottom-0 ">
         <Menubar>
         <MenubarMenu>
-          <MenubarTrigger>Ver</MenubarTrigger>
-          <MenubarTrigger><Plus /></MenubarTrigger>
+          <MenubarTrigger><Eye/> Meus processos</MenubarTrigger>
+          <Separator orientation="vertical"/>
+          <MenubarTrigger><Plus /> Adicionar</MenubarTrigger>
         </MenubarMenu>
       </Menubar>
+      </div>
     )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
-import Image from "next/image";
+import { Footer } from "./footer/footer";
+import { ProcessosTable } from "./processosTable/processosTable";
 
 export default function Home() {
   return (
-    <main className="flex w-100">
-
+    <main className="flex items-center justify-center flex-col">
+      <h1>Meus processos </h1>
+    <ProcessosTable/>
+    <Footer/>
     </main>
   );
 }
