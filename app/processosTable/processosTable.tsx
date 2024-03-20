9cdd49e18a6f16f48086061f978a456750281dee
João Marcos <jmcsjoaomarcos@gmail.com>
2024-03-20 11:55:20 +0000

    feat: Corrigido o probela do prisma client adicionando prisma cached

--- app/processosTable/processosTable.tsx
+++ app/processosTable/processosTable.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import {
     Table,
     TableBody,
@@ -8,55 +10,24 @@ import {
     TableHeader,
     TableRow,
 } from "@/components/ui/table"
-
-
-const invoices = [
-    {
-        invoice: "INV001",
-        paymentStatus: "Paid",
-        totalAmount: "$250.00",
-        paymentMethod: "Credit Card",
-    },
-    {
-        invoice: "INV002",
-        paymentStatus: "Pending",
-        totalAmount: "$150.00",
-        paymentMethod: "PayPal",
-    },
-    {
-        invoice: "INV003",
-        paymentStatus: "Unpaid",
-        totalAmount: "$350.00",
-        paymentMethod: "Bank Transfer",
-    },
-    {
-        invoice: "INV004",
-        paymentStatus: "Paid",
-        totalAmount: "$450.00",
-        paymentMethod: "Credit Card",
-    },
-    {
-        invoice: "INV005",
-        paymentStatus: "Paid",
-        totalAmount: "$550.00",
-        paymentMethod: "PayPal",
-    },
-    {
-        invoice: "INV006",
-        paymentStatus: "Pending",
-        totalAmount: "$200.00",
-        paymentMethod: "Bank Transfer",
-    },
-    {
-        invoice: "INV007",
-        paymentStatus: "Pending",
-        totalAmount: "$300.00",
-        paymentMethod: "Credit Card",
-    },
-]
+import { db } from "../utils/db"
+import { useEffect, useState } from "react";
 
 
 export const ProcessosTable = () => {
+
+    const [vagas, setVagas] = useState([{}]);
+
+    useEffect(()=>{
+        async function fetchData() {
+            try {
+              const result = await db.vagas.findMany();
+              setVagas(result);
+            } catch (error) {
+              console.error('Erro ao buscar os dados:', error);
+            }
+    }},[vagas])
+
     return (
     <Table>
         <TableHeader>
@@ -65,16 +36,17 @@ export const ProcessosTable = () => {
                 <TableHead>Status</TableHead>
                 <TableHead>Link</TableHead>
             </TableRow>
+            {/* {vagas.map((vaga) => {
+                <TableRow key={vaga.nome}>
+                <TableCell className="font-medium">{}</TableCell>
+                <TableCell>{}</TableCell>
+                <TableCell>{}</TableCell>
+                <TableCell className="text-right">{}</TableCell>
+              </TableRow>
+            })} */}
         </TableHeader>
         <TableBody>
-            {invoices.map((invoice) => (
-                <TableRow key={invoice.invoice}>
-                    <TableCell className="font-medium">{invoice.invoice}</TableCell>
-                    <TableCell>{invoice.paymentStatus}</TableCell>
-                    <TableCell>{invoice.paymentMethod}</TableCell>
-                    <TableCell className="text-right">{invoice.totalAmount}</TableCell>
-                </TableRow>
-            ))}
+            
         </TableBody>
     </Table>
     )
