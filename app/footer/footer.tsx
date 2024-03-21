e7f588797cf5919cf4a9ada0dc286b1f404ea607
João Marcos <jmcsjoaomarcos@gmail.com>
2024-03-21 20:23:44 +0000

    feat: Border raius

--- app/footer/footer.tsx
+++ app/footer/footer.tsx
@@ -33,7 +33,7 @@ import { Separator } from "@/components/ui/separator"
 export const Footer = () => {
 
   return (
-    <div className="w-full justify-around rounded-s-full mx-10 items-center  shadow-inner py-2 px-6 flex fixed  bottom-0 bg-gray-100">
+    <div className="w-full justify-around mx-10 items-center border-2 border-slate-200	 py-2 px-6 flex fixed  bottom-0 bg-gray-100">
       <button className="flex justify-center items-center font-medium flex-col px-1 transition text-xs hover:scale-105 hover:text-green-500	 focus:text-green-500"><Eye/>Ver</button>
       <Separator orientation="vertical" />
       <Dialog>
